f69fcaf4c5df593ec4d1784da9ab13312414a581
talhaffthedark <sunkieditz@gmail.com>
2025-10-10 08:29:08 +0500

    Update ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -32,7 +32,7 @@ async (conn, mek, m, { from, quoted, sender, reply }) => {
         const end = new Date().getTime();
         const responseTime = (end - start) / 1000;
 
-        const text = `> *𝐀𝐫𝐬𝐥𝐚𝐧_𝐌𝐃 SPEED: ${responseTime.toFixed(2)}ms ${reactionEmoji}*`;
+        const text = `> *𝑷𝑹𝑶𝑭𝑬𝑺𝑺𝑶𝑹 𝑻𝑨𝑳𝑯𝑨 𝑴𝑫 ❤‍🔥: ${responseTime.toFixed(2)}ms ${reactionEmoji}*`;
 
         await conn.sendMessage(from, {
             text,
@@ -42,7 +42,7 @@ async (conn, mek, m, { from, quoted, sender, reply }) => {
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
                     newsletterJid: '120363348739987203@newsletter',
-                    newsletterName: "𝐀𝐫𝐬𝐥𝐚𝐧_𝐌𝐃",
+                    newsletterName: "𝑷𝑹𝑶𝑭𝑬𝑺𝑺𝑶𝑹 𝑻𝑨𝑳𝑯𝑨 𝑴𝑫 ❤‍🔥",
                     serverMessageId: 143
                 }
             }
@@ -69,7 +69,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         const message = await conn.sendMessage(from, { text: '*PINGING...*' })
         const endTime = Date.now()
         const ping = endTime - startTime
-        await conn.sendMessage(from, { text: `*🔥 𝐀𝐫𝐬𝐥𝐚𝐧_𝐌𝐃 SPEED : ${ping}ms*` }, { quoted: message })
+        await conn.sendMessage(from, { text: `*𝑷𝑹𝑶𝑭𝑬𝑺𝑺𝑶𝑹 𝑻𝑨𝑳𝑯𝑨 𝑴𝑫 ❤‍🔥 SPEED : ${ping}ms*` }, { quoted: message })
     } catch (e) {
         console.log(e)
         reply(`${e}`)
